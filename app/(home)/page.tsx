4e0e9eb86a495451b4b10c9e14543f4bbe41da94
Rahul A Mistry <149420892+ProdigyRahul@users.noreply.github.com>
2024-06-13 17:59:51 +0530

    Production Commit

---
+++ app/(home)/page.tsx
@@ -0,0 +1,18 @@
+import ActiveProducts from "@/components/active-products";
+import { getActiveProducts } from "@/lib/server-actions";
+
+const Home = async () => {
+  const activeProducts = await getActiveProducts();
+
+  console.log(activeProducts, "approve projects here");
+
+  return (
+    <>
+      <div className="md:w-3/5 mx-auto py-10 px-6">
+        <ActiveProducts activeProducts={activeProducts} />
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ components/navbar/menu.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useState } from "react";
+import LaunchesMenu from "./menus/launches-menu";
+import Link from "next/link";
+import CommunityMenu from "./menus/community-menu";
+import AboutMenu from "./menus/about-menu";
+
+const Menu = () => {
+  const [showLaunchesMenu, setShowLaunchesMenu] = useState(false);
+  const [showCommunityMenu, setShowCommunityMenu] = useState(false);
+  const [showAboutMenu, setShowAboutMenu] = useState(false);
+  const textDisplay = "Advertise  (Soon)";
+  return (
+    <div className="hidden lg:flex items-center relative">
+      <div className="space-x-6 text-gray-600 text-sm flex items-center cursor-pointer">
+        <Link href={"/categories"} className="hover:text-[#ff6154]">
+          Categories
+        </Link>
+
+        <div
+          onMouseEnter={() => setShowCommunityMenu(true)}
+          onMouseLeave={() => setShowCommunityMenu(false)}
+          className="hover:text-[#ff6154] py-4"
+        >
+          Community (Soon) {showCommunityMenu && <CommunityMenu />}
+        </div>
+
+        <div className="hover:text-[#ff6154] py-4"></div>
+        {textDisplay}
+        <div
+          onMouseEnter={() => setShowAboutMenu(true)}
+          onMouseLeave={() => setShowAboutMenu(false)}
+          className="hover:text-[#ff6154] py-4"
+        >
+          About (Soon) {showAboutMenu && <AboutMenu />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
